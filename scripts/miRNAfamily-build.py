5062a858e518233ea3db45ce4e5301a598dc5a0c
Exseivier Nope <exseivier@users.noreply.github.com>
2018-05-28 12:50:14 -0500

    Update miRNAfamily-build.py

--- scripts/miRNAfamily-build.py
+++ scripts/miRNAfamily-build.py
@@ -1,8 +1,63 @@
 #!/usr/bin/env python
 
 from sys import argv, exit, path
-path.append('/Users/javier/scripts/ban-4.0/utils')
-from Utils import load_Ome
+#path.append('/Users/javier/scripts/ban-4.0/utils') Commented because Utils function was copied and pasted in this script
+#from Utils import load_Ome
+
+def load_Ome(filename):
+	"""(STR) -> HASH[STR:STR]
+	Requires a fasta file name of the reference genome sequence
+	It takes the chromosomes of a genome and their sequences
+	and stores them into a HASH variable "HASH[header:sequence]"
+	"""
+	# Counting total headers
+	IN = open(filename, "r")
+	total = 0
+	chrs = {}
+	for line in IN:
+		line = line.strip()
+		try:
+			if line[0] == ">":
+				pass
+		except Exception:
+			continue
+		if line[0] == ">":
+			header = line
+			total += 1
+		else:
+			try:
+				chrs[header] += 1
+			except Exception:
+				chrs[header] = 1
+	IN.close()
+	# Declaring and Assigning GENOME HASH variable
+	OME = {}
+	counter = 0
+	IN = open(filename, "r")
+	for line in IN:
+		line = line.strip()
+		try:
+			if line[0] == ">":
+				pass
+		except Exception:
+			continue
+		if line[0] == ">":
+			counter += 1
+			print "\rParsing %s [%f" % (line, (float(counter)/float(total))*100) + " %]"
+			header = line
+		else:
+			try:
+				OME[header] += line
+			except Exception:
+				OME[header] = line
+	IN.close()
+	# Dumping HASH variable to a file
+	if len(OME) != 0:
+		# Returning HASH
+		return OME
+	else:
+		print "'Ome' is empty!"
+		exit(0)
 
 def miR_2_TS(infile):
 	"""(STR) -> VOID (Writes to file)
